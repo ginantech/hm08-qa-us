3a987a542edcc78d266e550943d7ca68f343244c
Ethan Jacobsson <ezrahyte@gmail.com>
2024-08-24 17:46:24 -0700

    files

---
+++ wdio.conf.js
@@ -0,0 +1,37 @@
+exports.config = {
+    runner: 'local',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    headless: true,
+    capabilities: [
+    {
+        maxInstances: 5,
+        browserName: 'firefox',
+        acceptInsecureCerts: true,
+        'moz:firefoxOptions': {
+            //args: ['-headless']
+        }
+    }
+],
+    logLevel: 'error',
+    bail: 0,
+    baseUrl: 'https://cnt-d22caaf9-5160-4eaa-814f-73d4dfe82a07.containerhub.tripleten-services.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [ 
+        'geckodriver', 
+        'intercept', 
+    ],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
